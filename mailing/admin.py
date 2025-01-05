a3048de87bd34d5ea8804f8889e40bc03120e82e
marinabrovko <marishka.brovko@gmail.com>
2025-01-05 22:03:59 +0300

    register mes

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from mailing.models import Recipient
+from mailing.models import Recipient, Message
 
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
     list_display = ("full_name", "email")
     search_fields = ('full_name', 'email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("topic",)
+    ordering = ("topic",)
+    search_fields = ("topic", "text")
