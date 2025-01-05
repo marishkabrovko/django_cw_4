016307cfdaef8784922b89d0034e656b9da17584
marinabrovko <marishka.brovko@gmail.com>
2025-01-05 23:17:09 +0300

    add owner

--- mailing/models.py
+++ mailing/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+from users.models import User
+
 
 class Recipient(models.Model):
     email = models.EmailField(unique=True, verbose_name="Email", help_text="Введите электронную почту")
     full_name = models.CharField(max_length=150, verbose_name="ФИО", help_text="Введите ФИО")
     comment = models.TextField(verbose_name="Комментарий", help_text="Напишите комментарий", blank=True, null=True)
+
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Владелец", blank=True, null=True,
+                              related_name="recipients")
 
     def __str__(self):
         return self.email
@@ -30,8 +35,7 @@ class Message(models.Model):
 
 class Mailing(models.Model):
     first_sending = models.DateTimeField(verbose_name="Дата первой рассылки", auto_now_add=True)
-    end_sending = models.DateTimeField(verbose_name="Дата окончания рассылки",
-                                       help_text="Введите дату окончания рассылки", blank=True, null=True)
+    end_sending = models.DateTimeField(verbose_name="Дата окончания рассылки", blank=True, null=True)
 
     STATUS_OF_MAILING = [
         ("created", "Создана"),
@@ -43,6 +47,8 @@ class Mailing(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name="Сообщение",
                                 help_text="Выберите сообщение")
     recipients = models.ManyToManyField(Recipient, verbose_name="Получатели", help_text="Выберите получателей")
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Владелец рассылки", blank=True, null=True,
+                              related_name="mailings")
 
     def __str__(self):
         return f"Рассылка №{self.pk}"
@@ -54,15 +60,16 @@ class Mailing(models.Model):
 
 
 class AttemptSending(models.Model):
-    time = models.DateTimeField(verbose_name="Дата и время попытки", auto_now=True)
+    time = models.DateTimeField(verbose_name="Дата и время попытки", auto_now_add=True)
 
     STATUS_OF_ATTEMPT = [
         ('success', 'Успешно'),
         ('not_success', 'Не успешно'),
     ]
     status = models.CharField(choices=STATUS_OF_ATTEMPT, default='not_success', verbose_name="Статус попытки")
-    response = models.TextField(verbose_name="Ответ почтового сервера")
-    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name="Рассылка")
+    response = models.TextField(verbose_name="Ответ почтового сервера", null=True, blank=True)
+    mailing = models.ForeignKey(Mailing, on_delete=models.SET_NULL, verbose_name="Рассылка", null=True, blank=True,
+                                related_name="attempts")
 
     def __str__(self):
         return f'Попытка №{self.pk}'
